641c31f7ba1faea107232ac2e689c6c67e99f6af
Avijit <avijitb8888@gmail.com>
2026-02-03 10:19:46 +0530

    updated

---
+++ script3.js
@@ -0,0 +1,22 @@
+debugger;
+console.log("Hello, World!");
+console.log(a)
+
+var a=678
+let b=567
+
+console.log(a)
+console.log(b)
+
+function print(){
+    let c=63
+    console.log(c)
+    console.log("inside fn")
+
+}
+
+print()
+
+function first(){
+    ScriptProc
+}
