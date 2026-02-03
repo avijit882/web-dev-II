67ebb8672b7b21e6435a0e6e613e5ef8a4e3c52a
Avijit <avijitb8888@gmail.com>
2026-02-03 15:01:08 +0530

    updated

---
+++ script4.js
@@ -0,0 +1,38 @@
+// console.log("Starting homework...");
+
+// setTimeout(() => {
+
+// console.log ("Homework done!");
+
+// console.log("Starting dinner...");
+
+// setTimeout(() => {
+
+// console.log("Dinner done!");
+
+// console.log("Getting ready to go out...");
+
+// setTimeout(() => {
+
+// console.log("Going to the playground!");
+
+// }, 1000); // after dinner
+
+// }, 1500); // dinner time
+
+// }, 2000); // homework tim
+function finishHomework(callback ) {
+    console.log("starting homework...");
+    settimeout(()=>{
+        console.log("Homework done!");
+        Callback();
+    },1500);
+}
+ function gotoPlayground(){
+    console.log("Going to the Playground!");
+ } 
+ finishHomework(()=>{
+    eatdinner(()=>{
+        gotoPlayground();
+    });
+ });
