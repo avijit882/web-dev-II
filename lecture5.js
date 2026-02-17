581f968ff43f28fdc1fc2384a163e8fd94f9662f
Avijit <avijitb8888@gmail.com>
2026-02-17 12:33:25 +0530

    updated

---
+++ lecture5.js
@@ -0,0 +1,71 @@
+function orderFood() {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            console.log("Food Ordered")
+            res()
+        }, 2000)
+    })
+}
+
+function deliverFood(){
+    return new Promise((res,rej)=>
+    {        setTimeout(() => {
+            console.log("Food Delivered")
+            res()
+        }, 2000)
+    })
+}
+
+async function main(){
+    await orderFood()
+    await deliverFood()
+    console.log("Enjoy your meal")
+}
+
+orderFood()
+
+  orderFood().then(() => {
+    return deliverFood()
+  }).then(() => {
+    return("Enjoy your meal")
+  }).then((message) => {
+    console.log(message)
+  })
+
+main()
+
+order()
+
+orderFood().then((data)=>{
+    return PrepareFood()
+}).then((data)=>{
+    return deliverFood()
+}).then((data)=>{
+
+}).catch((error)=>{
+    console.log("something went wrong")
+})
+
+console.log("First Line")
+try{
+    let sample=234
+    console.log(sample)
+    console.log("Line after sample")
+    let age=16
+if (age<18){
+    throw new error("You are not eligible to vote")
+    // 
+    // 
+    // 
+    // 
+    // 
+    // 
+}
+// 
+
+} catch (e) {
+    console.log(e)
+}
+console.log("Last Line")
+
+ 
